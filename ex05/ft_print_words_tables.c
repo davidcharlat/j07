e3d03f3a7da2d44ffcfb2138fd9824057b11d1f5
Thomas Charlat <thomas.charlat@gmail.com>
2018-08-25 18:53:51 +0200

    Update ft_print_words_tables.c

--- ex05/ft_print_words_tables.c
+++ ex05/ft_print_words_tables.c
@@ -2,16 +2,11 @@ void	ft_putchar(char c);
 
 void	ft_print_words_tables(char **tab)
 {
-	int tab_cursor;
-    int letter_cursor;
-    
-    tab_cursor = 0;
-    while (tab[tab_cursor])
-    {
-		letter_cursor = 0;
-		while(tab[tab_cursor][letter_cursor])
-			ft_putchar(tab[tab_cursor][letter_cursor++]);
+	while (*tab)
+	{
+		while(**tab)
+			ft_putchar(**tab++);
 		ft_putchar('\n'); 
- 		tab_cursor++;   
-    }
+		*tab++;
+	}
 }
